c33b60b7cd51bd03e38f8dc68952bafe2febc78f
Tarique <tarique@localhost.localdomain>
2024-06-30 17:26:52 +0530

    change

--- Send_Mail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
